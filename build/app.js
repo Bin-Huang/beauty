1892a5624b7d68bd9a19525d3995cb9397a1f940
git <980502459@qq.com>
2017-04-24 18:21:12 +0800

    index smooth

--- build/app.js
+++ build/app.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { StyleProvider, Container, Content, Footer, FooterTab, Left, Body, Right, Button, Icon, Title } from "native-base";
 import ChatScreen from "./chat";
 import ChildPage from "./child";
 import PersonScreen from "./person";
 import HomeScreen from "./home";
+import Swiper from "react-native-swiper";
+
+let currentIndex = 0;
 
 export default class App extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
             route: "index",
-            current: "home",
             more: <View />,
             title: "",
         };
         this.set = this.set.bind(this);
         this.back = this.back.bind(this);
+        this.go = this.go.bind(this);
     }
     set(obj) {
         this.setState(obj);
@@ -24,8 +27,11 @@ export default class App extends React.Component{
     back() {
         this.setState({
             route: "index",
-            current: this.state.current,
         });
+    }
+    go(index) {
+        this.viewPager.scrollBy(index - currentIndex, true);
+        currentIndex = index;
     }
     render() {
         if (this.state.route !== "index") {
@@ -35,39 +41,38 @@ export default class App extends React.Component{
                 </ChildPage>
             );
         } else {
-            let MyContent = Content;
-            if (this.state.current === "chat") {
-                MyContent = ChatScreen;
-            } else if (this.state.current === "person") {
-                MyContent = PersonScreen;
-            } else if (this.state.current === "home") {
-                MyContent = HomeScreen;
-            }
             return (
                 <Container>
-                    <MyContent set={this.set} />
-                    <Footer>
-                        <FooterTab  >
-                            <Button  active={this.state.current === "home" ? true : false}
-                            onPress={() => {this.setState({current: "home"}); }}>
-                                <Icon name="paper" />
-                            </Button>
-                            <Button  active={this.state.current === "camera" ? true : false}
-                            onPress={() => {this.setState({current: "camera"}); }}>
-                                <Icon name="camera" />
-                            </Button>
-                            <Button  active={this.state.current === "chat" ? true : false}
-                            onPress={() => {this.setState({current: "chat"}); }}>
-                                <Icon name="chatboxes" />
-                            </Button>
-                            <Button active={this.state.current === "person" ? true : false}
-                            onPress={() => {this.setState({current: "person"}); }}>
-                                <Icon name="person" />
-                            </Button>
-                        </FooterTab>
-                    </Footer>
+                    <Content>
+                        <Swiper bounces={true } showsButtons={false} index={currentIndex} scrollEnabled={false} showsPagination={false} loop={true} ref={viewPager => { this.viewPager = viewPager; }} >
+                            <HomeScreen set={this.set} />
+                            <View>
+                                <Button onPress={this.go} style={{backgroundColor: "red"}} />
+                            </View>
+                            <ChatScreen set={this.set}/>
+                            <PersonScreen set={this.set}/>
+                        </Swiper>
+                    </Content>
+                    <View>
+                        <Footer>
+                            <FooterTab>
+                                <Button onPress={() => {
+                                    this.go(0);
+                                    }}><Icon name="paper" /></Button>
+                                <Button onPress={() => {
+                                    this.go(1);
+                                    }}><Icon name="camera" /></Button>
+                                <Button onPress={() => {
+                                    this.go(2);
+                                    }}><Icon name="chatboxes" /></Button>
+                                <Button onPress={() => {
+                                    this.go(3);
+                                    }}><Icon name="person" /></Button>
+                            </FooterTab>
+                        </Footer>
+                    </View>
                 </Container>
-            );
+            )
         }
     }
 }
